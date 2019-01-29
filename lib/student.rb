103b8b9760ad6ba205eeacba42945f3a22f17e0c
Steve Albonico <salbonico@gmail.com>
2019-01-29 23:31:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,10 @@ attr_accessor :name, :grade
 attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+def initialize(id=null,name,grade)
+@name = name
+@grade = grade
 
+end
 
 end
